44331628c25c159dc831328d661def445326e1ac
bilalk17 <47456128+bilalk17@users.noreply.github.com>
2026-04-25 15:05:15 -0400

    Update Single neuron forward pass (Build a Neural Net 1/5)

--- foundations/neuron.py
+++ foundations/neuron.py
@@ -16,6 +16,6 @@ class Solution:
         pre_activation = np.dot(x, w) + b
         if activation == "sigmoid":
             activation = 1 / (1 + np.exp(-pre_activation))
-        elif activation == "relu":
+        if activation == "relu":
             activation = np.maximum(0, pre_activation)
         return round(activation, 5)
